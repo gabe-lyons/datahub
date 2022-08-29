3c42ab5d6bd01b121d39acab9502799b6fa27d61
Ryan Holstien <ryan@acryl.io>
2022-08-29 17:22:24 -0500

    more clean up

--- datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/restorebackup/backupreader/ParquetEbeanAspectBackupIterator.java
+++ datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/restorebackup/backupreader/ParquetEbeanAspectBackupIterator.java
@@ -16,45 +16,6 @@ public class ParquetEbeanAspectBackupIterator implements EbeanAspectBackupIterat
   private final List<ParquetReaderWrapper> _parquetReaders;
   private int currentReaderIndex = 0;
 
-//  @Override
-//  public EbeanAspectV2 next(ParquetReader<GenericRecord> parquetReader) {
-//
-//    try {
-//      long readStart = System.nanoTime();
-//      GenericRecord record = parquetReader.read();
-//      long readEnd = System.nanoTime();
-//      totalTimeSpentInRead += readEnd - readStart;
-//
-//      while ((record != null) && ((Long) record.get("version") != 0L)) {
-//        recordsSkipped += 1;
-//        readStart = System.nanoTime();
-//        record = parquetReader.read();
-//        readEnd = System.nanoTime();
-//        totalTimeSpentInRead += readEnd - readStart;
-//      }
-//      if ((readEnd - lastTimeLogged) > 1000 * 1000 * 1000 * 5) {
-//        // print every 5 seconds
-//        printStat("Running: ");
-//        lastTimeLogged = readEnd;
-//      }
-//      if (record == null) {
-//        printStat("Closing: ");
-//        parquetReader.close();
-//        return null;
-//      }
-//      long convertStart = System.nanoTime();
-//      final EbeanAspectV2 ebeanAspectV2 = convertRecord(record);
-//      long convertEnd = System.nanoTime();
-//      this.totalTimeSpentInConvert += convertEnd - convertStart;
-//      this.recordsProcessed++;
-//      return ebeanAspectV2;
-//    } catch (Exception e) {
-//      log.error("Error while reading backed up aspect", e);
-//      this.recordsFailed++;
-//      return null;
-//    }
-//  }
-
   @Override
   public ParquetReaderWrapper getNextReader() {
       if (currentReaderIndex >= _parquetReaders.size()) {
