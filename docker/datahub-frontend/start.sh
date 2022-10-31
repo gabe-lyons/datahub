62e63a4eb585b6371ab01a58d9d34669b006b605
Chris Collins <chriscollins@Chriss-MBP-2.lan>
2022-10-31 17:49:49 -0400

    update docker/datahub-frontend/start.sh with correct conflict resolutions

--- docker/datahub-frontend/start.sh
+++ docker/datahub-frontend/start.sh
@@ -11,4 +11,33 @@ fi
 # Remove empty newlines, if there are any.
 sed '/^[[:space:]]*$/d' /datahub-frontend/conf/tmp.props > /datahub-frontend/conf/user.props
 rm /datahub-frontend/conf/tmp.props
-/datahub-frontend/bin/datahub-frontend
+
+TRUSTSTORE_FILE=""
+if [[ ! -z ${SSL_TRUSTSTORE_FILE:-} ]]; then
+  TRUSTSTORE_FILE="-Djavax.net.ssl.trustStore=$SSL_TRUSTSTORE_FILE"
+fi
+
+TRUSTSTORE_TYPE=""
+if [[ ! -z ${SSL_TRUSTSTORE_TYPE:-} ]]; then
+  TRUSTSTORE_TYPE="-Djavax.net.ssl.trustStoreType=$SSL_TRUSTSTORE_TYPE"
+fi
+
+TRUSTSTORE_PASSWORD=""
+if [[ ! -z ${SSL_TRUSTSTORE_PASSWORD:-} ]]; then
+  TRUSTSTORE_PASSWORD="-Djavax.net.ssl.trustStorePassword=$SSL_TRUSTSTORE_PASSWORD"
+fi
+
+# make sure there is no whitespace at the beginning and the end of 
+# this string
+export JAVA_OPTS="-Xms512m \
+   -Xmx1024m \
+   -Dhttp.port=$SERVER_PORT \
+   -Dconfig.file=datahub-frontend/conf/application.conf \
+   -Djava.security.auth.login.config=datahub-frontend/conf/jaas.conf \
+   -Dlogback.configurationFile=datahub-frontend/conf/logback.xml \
+   -Dlogback.debug=false \
+   ${PROMETHEUS_AGENT:-} ${OTEL_AGENT:-} \
+   ${TRUSTSTORE_FILE:-} ${TRUSTSTORE_TYPE:-} ${TRUSTSTORE_PASSWORD:-} \
+   -Dpidfile.path=/dev/null"
+
+exec ./datahub-frontend/bin/datahub-frontend
