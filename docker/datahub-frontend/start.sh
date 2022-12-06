7df3c4fbc26e3815df04b340d1f886467fd94843
Chris Collins <chriscollins3456@gmail.com>
2022-12-06 10:42:37 -0500

    fix broken merge from master on frontend's start.sh file

--- docker/datahub-frontend/start.sh
+++ docker/datahub-frontend/start.sh
@@ -5,12 +5,16 @@ touch /datahub-frontend/conf/user.props
 touch /datahub-frontend/conf/tmp.props
 
 echo "admin:${ADMIN_PASSWORD}" >> /datahub-frontend/conf/tmp.props
+
 if [ -n "${CUSTOM_USER_PROPS_FILE}" ]; then
   cat "${CUSTOM_USER_PROPS_FILE}" >> /datahub-frontend/conf/tmp.props
+fi
+
 OTEL_AGENT=""
 if [[ ${ENABLE_OTEL:-false} == true ]]; then
   OTEL_AGENT="-javaagent:/opentelemetry-javaagent.jar"
 fi
+
 # Remove empty newlines, if there are any.
 sed '/^[[:space:]]*$/d' /datahub-frontend/conf/tmp.props > /datahub-frontend/conf/user.props
 rm /datahub-frontend/conf/tmp.props
