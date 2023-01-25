7650a77cbcdcb94e33ea7011b358e8fc0477f275
Aseem Bansal <asmbansal2@gmail.com>
2023-01-25 15:46:36 +0530

    feat(monitoring): add sentry for monitoring gms (#1107)

--- metadata-service/factories/src/main/java/com/linkedin/metadata/boot/OnBootApplicationListener.java
+++ metadata-service/factories/src/main/java/com/linkedin/metadata/boot/OnBootApplicationListener.java
@@ -1,9 +1,13 @@
 package com.linkedin.metadata.boot;
 
 import javax.annotation.Nonnull;
+
+import com.linkedin.metadata.version.GitVersion;
+import io.sentry.Sentry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -23,9 +27,41 @@ public class OnBootApplicationListener implements ApplicationListener<ContextRef
   @Qualifier("bootstrapManager")
   private BootstrapManager _bootstrapManager;
 
+  @Autowired
+  @Qualifier("gitVersion")
+  private GitVersion gitVersion;
+
+  @Value("${sentry.enabled}")
+  private Boolean sentryEnabled;
+
+  @Value("${sentry.dsn}")
+  private String sentryDsn;
+
+  @Value("${sentry.env}")
+  private String sentryEnv;
+
+  @Value("${sentry.debug}")
+  private Boolean sentryDebug;
+
   @Override
   public void onApplicationEvent(@Nonnull ContextRefreshedEvent event) {
     if (ROOT_WEB_APPLICATION_CONTEXT_ID.equals(event.getApplicationContext().getId())) {
+      if (sentryEnabled) {
+        Sentry.init(options -> {
+          options.setDsn(sentryDsn);
+          options.setRelease(gitVersion.getVersion());
+          options.setEnvironment(sentryEnv);
+          options.setTracesSampleRate(0.0);
+          options.setDebug(sentryDebug);
+        });
+        if (sentryDebug) {
+          try {
+            throw new Exception("This is a test.");
+          } catch (Exception e) {
+            Sentry.captureException(e);
+          }
+        }
+      }
       _bootstrapManager.start();
     }
   }
