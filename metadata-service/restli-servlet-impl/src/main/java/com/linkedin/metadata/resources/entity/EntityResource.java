3c265d524d8c53062769637708c7a5d88b9dabe7
Pedro Silva <pedro@acryl.io>
2023-02-28 18:49:53 +0000

    Fix log in delete for EntityResource

--- metadata-service/restli-servlet-impl/src/main/java/com/linkedin/metadata/resources/entity/EntityResource.java
+++ metadata-service/restli-servlet-impl/src/main/java/com/linkedin/metadata/resources/entity/EntityResource.java
@@ -452,7 +452,7 @@ public class EntityResource extends CollectionResourceTaskTemplate<String, Entit
               EntitySpecUtils.getEntityTimeseriesAspectNames(_entityService.getEntityRegistry(), urn.getEntityType());
       if (aspectName != null && !timeseriesAspectNames.contains(aspectName)) {
         throw new UnsupportedOperationException(
-                String.format("Not supported for non-timeseries aspect '{}'.", aspectName));
+                String.format("Not supported for non-timeseries aspect %s.", aspectName));
       }
       List<String> timeseriesAspectsToDelete =
               (aspectName == null) ? timeseriesAspectNames : ImmutableList.of(aspectName);
