feec4c6f0b2af51709090796f9bf4b8d67fd65ec
John Joyce <john@acryl.io>
2023-04-04 19:29:50 -0700

    refactor(Tests): Metadata Tests Uplift: Adding Empty Tests state (#1262)

--- datahub-web-react/src/app/tests/Tests.tsx
+++ datahub-web-react/src/app/tests/Tests.tsx
@@ -4,6 +4,7 @@ import { groupTestsByCategory } from './utils';
 import { TestsSection } from './TestsSection';
 import { Test } from '../../types.generated';
 import { ANTD_GRAY } from '../entity/shared/constants';
+import EmptyTests from './EmptyTests';
 
 const Container = styled.div`
     padding-top 28px;
@@ -16,19 +17,21 @@ type Props = {
 };
 
 export const Tests = ({ tests }: Props) => {
+    const hasTests = tests.length > 0;
     const testSections = groupTestsByCategory(tests);
     return (
         <Container>
-            {testSections.map((section) => {
-                return (
-                    <TestsSection
-                        title={section.name}
-                        tooltip={section.description}
-                        tests={section.tests}
-                        key={section.name || ''}
-                    />
-                );
-            })}
+            {(hasTests &&
+                testSections.map((section) => {
+                    return (
+                        <TestsSection
+                            title={section.name}
+                            tooltip={section.description}
+                            tests={section.tests}
+                            key={section.name || ''}
+                        />
+                    );
+                })) || <EmptyTests />}
         </Container>
     );
 };
