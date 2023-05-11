6a3564ec28ae2cb608153e768a57e41f2dc8dc97
Harshal Sheth <hsheth2@gmail.com>
2023-05-11 12:40:40 -0700

    fix lint

--- datahub-integrations-service/tests/conftest.py
+++ datahub-integrations-service/tests/conftest.py
@@ -8,4 +8,4 @@ with unittest.mock.patch("datahub.ingestion.graph.client.DataHubGraph") as mock:
 
     # The app module initializes a global graph object. We need to mock that object
     # here so that it doesn't fail when testing it's connection.
-    import datahub_integrations.app
+    import datahub_integrations.app  # noqa: F401
