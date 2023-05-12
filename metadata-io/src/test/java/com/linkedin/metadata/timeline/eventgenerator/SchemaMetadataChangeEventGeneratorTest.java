4db0e36bc20ca944116ee3dfe1a209cb58e8e49c
david-leifker <114954101+david-leifker@users.noreply.github.com>
2023-05-12 16:16:02 -0500

    remove assert in production code (#1342)

---
+++ metadata-io/src/test/java/com/linkedin/metadata/timeline/eventgenerator/SchemaMetadataChangeEventGeneratorTest.java
@@ -0,0 +1,6 @@
+package com.linkedin.metadata.timeline.eventgenerator;
+
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+public class SchemaMetadataChangeEventGeneratorTest extends AbstractTestNGSpringContextTests {
+}
